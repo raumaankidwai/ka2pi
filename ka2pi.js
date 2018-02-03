ccc0355e3aa84665fd55fe7f79182dfe6830384f
Raumaan Kidwai <raumaankidwai@gmail.com>
2018-02-03 12:22:31 -0500

    Update ka2pi.js

--- ka2pi.js
+++ ka2pi.js
@@ -4,7 +4,7 @@ const { URL } = require("url");
 
 const { KEY, SECRET, PORT } = require("./options.json");
 
-const CALLBACK = "http://localhost:7565/callback";
+const CALLBACK = `http://localhost:${PORT}/callback`;
 
 const KA2PI = new OAuth(
 	`https://www.khanacademy.org/api/auth2/request_token?oauth_callback=${CALLBACK}`,
